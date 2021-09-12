61aade9415848191d6fc1ebad5c71c2b2ef5d1cf
Varun Malhotra <varun@edgeguard.io>
2021-09-12 15:14:33 -0700

    create async version of wifi scan method

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-    let rt = tokio::runtime::Runtime::new().expect("could not initialise runtime");
-    let f = rt.spawn(async { wifiscanner::scan().await });
-
-    let networks = rt.block_on(f).expect("cannot scan network");
-    for network in networks.unwrap_or_default() {
+    let networks = wifiscanner::scan().expect("Cannot scan network");
+    for network in networks {
         println!(
             "{} {:15} {:10} {:4} {}",
             network.mac, network.ssid, network.channel, network.signal_level, network.security
